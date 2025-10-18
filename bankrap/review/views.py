8c58246985bbadf43c039a62cbc4fbb667d53c76
gumap <gumap@MSI>
2025-10-19 01:37:24 +0800

    Add review URL and view

--- bankrap/review/views.py
+++ bankrap/review/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from .models import ReviewAndRating
 
-# Create your views here.
+class ReviewView(View):
+    template_name = 'review.html'
+
+    def get(self, request):
+        reviews = ReviewAndRating.objects.all()
+        return render(request, self.template_name, {'reviews': reviews})
