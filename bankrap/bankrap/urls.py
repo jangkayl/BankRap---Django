6c50dfe5690a3e6352280096b9cc5b3750902a53
Pallu <159033105+Palludia@users.noreply.github.com>
2025-10-19 16:06:26 +0800

    Added Transaction

--- bankrap/bankrap/urls.py
+++ bankrap/bankrap/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('review/', include('review.urls'))
+    path('review/', include('review.urls')),
+    path('transaction/', include('transaction.urls'))
 ]
